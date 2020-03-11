b80b55032f232505d88d24f41de498527c139be6
antares-va-tech <antares.va.tech@gmail.com>
2020-03-10 22:50:04 -0400

    removed unused import and format

--- react/skynet-appstore/src/components/sn.register.js
+++ react/skynet-appstore/src/components/sn.register.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
@@ -24,169 +23,171 @@ const useStyles = theme => ({
 });
 
 class SnRegister extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       skyapp: {
-        title: '',
-        description: '',
-        fileName: '',
-        fileFormat: '',
-        type: '',
-        category: '',
-        githubUrl: '',
+        title: "",
+        description: "",
+        fileName: "",
+        fileFormat: "",
+        type: "",
+        category: "",
+        githubUrl: "",
         searchable: true
       }
     };
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange (evt) {
+  handleChange(evt) {
     console.log(evt.target.type);
     const eleType = evt.target.type;
     const skyapp = this.state.skyapp;
-    if (eleType==='checkbox'){
+    if (eleType === "checkbox") {
       skyapp[evt.target.name] = evt.target.checked;
     } else {
       skyapp[evt.target.name] = evt.target.value;
     }
-    this.setState({skyapp});
+    this.setState({ skyapp });
   }
 
   render() {
     const { classes } = this.props;
     const { skyapp } = this.state;
-  return (
-    <div className="container-fluid register-container">
-      <div className="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 className="h3 mb-0 text-gray-800">Register SkyApp</h1>
-      </div>
-      {/* <Typography variant="h6" gutterBottom>
-                Register SkyApp
-              </Typography> */}
-      <Grid container spacing={5}>
-        <Grid item xs={12}>
-          <TextField
-            required
-            id="title"
-            name="title"
-            label="Title"
-            fullWidth
-            autoComplete="off"
-            helperText="Max 15 charecters"
-            onChange={this.handleChange}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            required
-            id="description"
-            name="description"
-            label="Description"
-            fullWidth
-            autoComplete="off"
-            helperText="Max 200 charecters"
-            onChange={this.handleChange}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            required
-            id="fileName"
-            name="fileName"
-            label="File Name"
-            fullWidth
-            autoComplete="off"
-            onChange={this.handleChange}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            required
-            id="fileFormat"
-            name="fileFormat"
-            label="File Format"
-            fullWidth
-            autoComplete="off"
-            onChange={this.handleChange}
-          />
-        </Grid>
-        <Grid item xs={6} className="select-grid">
-          <FormControl className={classes.formControl}>
-            <InputLabel id="demo-simple-select-label">Type</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              fullWidth
-              name="type"
-              onChange={this.handleChange}
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value={10}>Directory</MenuItem>
-              <MenuItem value={20}>File</MenuItem>
-            </Select>
-          </FormControl>
-        </Grid>
-        <Grid item xs={6} className="select-grid">
-          <FormControl className={classes.formControl}>
-            <InputLabel id="demo-simple-select-label">Category</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="category"
-              fullWidth
-              name="category"
-              onChange={this.handleChange}
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value={10}>Video</MenuItem>
-              <MenuItem value={20}>Audio</MenuItem>
-              <MenuItem value={20}>Blog</MenuItem>
-              <MenuItem value={20}>Dataset</MenuItem>
-              <MenuItem value={20}>SkyApp</MenuItem>
-            </Select>
-          </FormControl>
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            id="githubUrl"
-            name="githubUrl"
-            label="Github URL"
-            fullWidth
-            autoComplete="off"
-            onChange={this.handleChange}
-          />
-        </Grid>
-        <Grid item xs={6} className="paddingt-40">
-          <FormControlLabel
-            control={
-              <Checkbox color="secondary" 
+    return (
+      <div className="container-fluid register-container">
+        <div className="d-sm-flex align-items-center justify-content-between mb-4">
+          <h1 className="h3 mb-0 text-gray-800">Register SkyApp</h1>
+        </div>
+        <form>
+          <Grid container spacing={5}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                id="title"
+                name="title"
+                label="Title"
+                fullWidth
+                autoComplete="off"
+                helperText="Max 15 charecters"
+                onChange={this.handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                id="description"
+                name="description"
+                label="Description"
+                fullWidth
+                autoComplete="off"
+                helperText="Max 200 charecters"
+                onChange={this.handleChange}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                required
+                id="fileName"
+                name="fileName"
+                label="File Name"
+                fullWidth
+                autoComplete="off"
+                onChange={this.handleChange}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                required
+                id="fileFormat"
+                name="fileFormat"
+                label="File Format"
+                fullWidth
+                autoComplete="off"
+                onChange={this.handleChange}
+              />
+            </Grid>
+            <Grid item xs={6} className="select-grid">
+              <FormControl className={classes.formControl}>
+                <InputLabel id="demo-simple-select-label">Type</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  fullWidth
+                  value={skyapp.type}
+                  name="type"
+                  onChange={this.handleChange}
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Directory</MenuItem>
+                  <MenuItem value={20}>File</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={6} className="select-grid">
+              <FormControl className={classes.formControl}>
+                <InputLabel id="demo-simple-select-label">Category</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="category"
+                  fullWidth
+                  value={skyapp.category}
+                  name="category"
+                  onChange={this.handleChange}
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Video</MenuItem>
+                  <MenuItem value={20}>Audio</MenuItem>
+                  <MenuItem value={20}>Blog</MenuItem>
+                  <MenuItem value={20}>Dataset</MenuItem>
+                  <MenuItem value={20}>SkyApp</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                id="githubUrl"
+                name="githubUrl"
+                label="Github URL"
+                fullWidth
+                autoComplete="off"
+                onChange={this.handleChange}
+              />
+            </Grid>
+            <Grid item xs={6} className="paddingt-40">
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    color="secondary"
                     name="searchable"
                     checked={skyapp.searchable}
-                    onChange={this.handleChange}/>
-            }
-            label="Searchable"
-          />
-        </Grid>
-        <Grid item xs={12} className="button-grid">
-          <Button
-            variant="contained"
-            color="primary"
-            className="btn-20px"
-            startIcon={<SaveIcon />}
-          >
-            Save
-          </Button>
-        </Grid>
-      </Grid>
-    </div>
-  );
+                    onChange={this.handleChange}
+                  />
+                }
+                label="Searchable"
+              />
+            </Grid>
+            <Grid item xs={12} className="button-grid">
+              <Button
+                variant="contained"
+                color="primary"
+                className="btn-20px"
+                type="submit"
+                startIcon={<SaveIcon />}
+              >
+                Save
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    );
   }
 }
 
-
-export default withStyles(useStyles)(SnRegister)
+export default withStyles(useStyles)(SnRegister);
